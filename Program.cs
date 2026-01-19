7bd8766a84711ddcf289b9112356cdfdaf74274b
thesaud <saud.person@gmail.com>
2026-01-19 11:50:06 +0300

    docker

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -26,4 +26,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Dashboards}/{action=Index}/{id?}");
 
-app.Run();
+app.Run("http://0.0.0.0:80");
